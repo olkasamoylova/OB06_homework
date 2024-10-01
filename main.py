2c17939acb3de210df36674be4c7b8fd527adf92
olkasamoylova <olga.n.samoylova@gmail.com>
2024-10-01 15:04:06 +0200

    add class Hero_methods

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+import random
+
+
 class Hero:
     def __init__(self, name):
-        self.name =  name
+        self.name = name
         self.health = 100
         self.attack_power = 20
 
-        
+    def attack(self, other):
+        damage = random.randint(10, self.attack_power)
+        other.health -= damage
+        print(f"{self.name} атакует {other.name} и наносит {damage} урона")
+
+    def is_alive(self):
+        return self.health > 0
+
+    
+
+
+
